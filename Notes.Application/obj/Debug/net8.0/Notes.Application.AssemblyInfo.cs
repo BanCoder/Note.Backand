708e14cfe3647d832995ee1d064197caeec018bc
Vladimir Belskiy <belskiy548@gmail.com>
2025-11-12 21:28:32 +0300

    Add Mediatr + CQRS patterns for making operations (create, update, delete)

--- Notes.Application/obj/Debug/net8.0/Notes.Application.AssemblyInfo.cs
+++ Notes.Application/obj/Debug/net8.0/Notes.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notes.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0685fc61be4f6de5bcfe09fb3bc8039f92634619")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e0fc1998b7be94e5a85891db98b28b4287a2d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notes.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notes.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
